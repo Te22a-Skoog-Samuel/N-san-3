4e6d3f50f6013ab399e1e0c538934909f020fd81
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2024-12-03 11:17:01 +0100

    jobbr på

---
+++ Näsan 3/Program.cs
@@ -0,0 +1,22 @@
+﻿void Hello32()
+{
+    for (int i = 0; 1 < 32; i++)
+    {
+        Console.WriteLine("Hello, World");
+    }
+}
+
+Hello32();
+
+
+
+void Square()
+{
+    Console.WriteLine("Välj en siffra.");
+    string tal = Console.ReadLine();
+    int resultat;
+    bool lyckad = int.TryParse(tal, out resultat);
+}
+
+
+Console.ReadKey();
